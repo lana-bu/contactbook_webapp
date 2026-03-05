5eb11b42afa4af08a98c2237ad381924b9281a01
lana-bu <lanabu@umich.edu>
2026-03-05 00:13:58 -0500

    no contacts message

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import ContactForm from './components/ContactForm.jsx';
 import ContactList from './components/ContactList.jsx';
 import SearchBar from './components/SearchBar.jsx';
 
+const contactData = [];
 
 function App() {
+
+  let contactsDisplay
+    if (contactData.length <= 0) {
+      contactsDisplay = <span>No contacts yet. Add one below!</span>;
+    } else {
+      contactsDisplay = <ContactList items={contactData} />;
+    }
+
   return (
     <>
       <SearchBar />
-      <ContactList />
-      <ContactForm />
+      <div>
+        {contactsDisplay}
+      </div>
     </>
   );
 }
