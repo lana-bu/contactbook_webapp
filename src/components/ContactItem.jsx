cfef9c9efc56f2ab7bf01dba49ee4ee3df7fe58c
lana-bu <lanabu@umich.edu>
2026-03-06 01:54:24 -0500

    added delete functionality

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -2,14 +2,17 @@
 // each contact includes id (Date.now().toString()), name (String, required), email (String, required), phone (String), maybe birthdate
 
 const ContactItem = function(props) {
+    const deleteButtonHandler = (event) => {
+        props.onDeleteContact(props.id);
+    }
+    
     return (
         <div class="list-item">
-            <span>{props.id}</span>
             <span>{props.name}</span>
             <span>{props.email}</span>
             <span>{props.phone}</span>
             <span>{props.birthdate}</span>
-            <button>Delete</button>
+            <button class="btn delete-btn" onClick={deleteButtonHandler}>Delete</button>
         </div>
     );
 }
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -3,6 +3,10 @@
 import ContactItem from './ContactItem.jsx';
 
 const ContactList = (props)=>{
+    const transferContactToDelete = (contactId) => {
+        props.contactToDelete(contactId);
+    }
+
     let contactsDisplay;
         if (props.allItems.length <= 0) { // no contacts added yet
             contactsDisplay = <span>No contacts yet. Add one below!</span>;
@@ -10,7 +14,7 @@ const ContactList = (props)=>{
             contactsDisplay = <span>No matches found for '{props.search}'.</span>;
         } else { // display matching contacts
             contactsDisplay = props.items.map(item => (
-                <ContactItem key={item.id} name={item.name} email={item.email} phone={item.phone} birthdate={item.birthdate} />
+                <ContactItem key={item.id} id={item.id} name={item.name} email={item.email} phone={item.phone} birthdate={item.birthdate} onDeleteContact={transferContactToDelete} /> // key is special react propery, can't be used by props
             ));
         }
 
