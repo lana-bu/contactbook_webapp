cbcb14ccfdbc951adb82730ab2d2d8f8306b5923
lana-bu <lanabu@umich.edu>
2026-03-07 00:15:22 -0500

    color styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import { BiSolidContact } from "react-icons/bi";
 
 function Header() {
     return (
-        <>
+        <div class='header'>
             <h1>Contact Book</h1>
             <BiSolidContact />
-        </>
+        </div>
     );
 }
 
