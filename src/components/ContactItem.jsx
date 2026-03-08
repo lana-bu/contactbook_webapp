98e19df61adb1cf0c7bb6cd36a5e2f52c7b42788
lana-bu <lanabu@umich.edu>
2026-03-08 01:01:23 -0500

    date format, mobile landscape layout

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -3,8 +3,28 @@
 
 import { IoMdContact } from "react-icons/io";
 import {FaTrashAlt} from 'react-icons/fa';
+import { format } from "date-fns"
+
+const defaultField = "(none)";
 
 const ContactItem = function(props) {
+    const setPhone = () => {
+        if (props.phone === '') {
+            return defaultField;
+        } else {
+            return props.phone
+        }
+    }
+
+    const setBirthdate = () => {
+        if (props.birthdate === '') {
+            return defaultField;
+        } else {
+            const [year, month, day] = props.birthdate.split('-');
+            return format(new Date(year, month - 1, day), "MMMM dd, yyyy");
+        }
+    }
+
     const deleteButtonHandler = (event) => {
         props.onDeleteContact(props.id);
     }
@@ -16,9 +36,9 @@ const ContactItem = function(props) {
                 <span className='contact-name'>{props.name}</span>
             </div>
             <div className='contact-info'>
-                <span className='contact-element contact-email'>{props.email}</span>
-                <span className='contact-element contact-phone'>{props.phone}</span>
-                <span className='contact-element contact-birthdate'>{props.birthdate}</span>
+                <span className='contact-element contact-email'>Email: {props.email}</span>
+                <span className='contact-element contact-phone'>Phone: {setPhone()}</span>
+                <span className='contact-element contact-birthdate'>Birthdate: {setBirthdate()}</span>
             </div>
             <button className="btn delete-btn" onClick={deleteButtonHandler}><FaTrashAlt /></button>
         </div>
